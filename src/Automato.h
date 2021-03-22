4e9c42574410ddfdb2135827185806fea15b8bdb
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2021-03-22 09:04:13 -0300

    Q1 solution

---
+++ src/Automato.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+
+using namespace std;
+
+// Matriz que define o automato
+vector<vector<char>> automata_matrix{{0x00, 'a', 'b', 0x00, 0x00}, 
+                                     {0x00, 'a', 0x00, 'c', 0x00},
+                                     {'b', 0x00, 0x00, 0x00, 0x00},
+                                     {0x00, 0x00, 'b', 'c', 'a'},
+                                     {'c', 'b', 'a', 0x00, 0x00}};
+
+// Vetor com as letras no automato
+vector<char> E{'a', 'b', 'c'};
+
+// Mascara de bits que mostra quais estados são marcados
+bitset<5> Xm = 0x19;
---
+++ src/q1_lista1a.cpp
@@ -0,0 +1,75 @@
+#include <string>
+#include <algorithm>
+#include "Automato.h"
+
+using namespace std;
+
+// Linguagens
+vector<string> Ln;
+vector<string> Lm;
+
+void ShowLanguage(vector<string> L){
+     cout << '{';
+    for(vector<string>::const_iterator i = L.begin(); i != L.end(); i++){
+        cout << *i << ' ';
+    }
+    cout << '}' << endl;
+}
+
+// Retorna se o estado é marcado
+bool IsMarked(int state){
+    if (((Xm >> state)&=0x01) == 0x01)
+        return true;
+    else return false;
+}
+
+// Essa função retornará a linguagem com palavras de N letras a partir de um estado dado
+void GetLanguageFromState(int N, int state = 0, string word_above =""){
+    for(int i = 0; i < N; i++){
+        int changes = 0;
+        for(int j = 0; j < automata_matrix[state].size(); j++){
+            char c = automata_matrix[state][j];
+            if(find(E.begin(), E.end(), c) != E.end()){
+                string word;
+                if(N == 1){
+                    word = word_above + c;
+                    if(find(Ln.begin(), Ln.end(), word) == Ln.end()){
+                        Ln.push_back(word);
+                        if(IsMarked(j))
+                            Lm.push_back(word);
+                    }
+                }
+                else
+                    GetLanguageFromState(N-1, j, word_above + c);
+            }
+        }
+    }
+}
+
+int main()
+{
+    bool ans = true;
+    do{
+        string input;
+        int N;
+        cout << "Entre com o tamanho N das palavras para para obter a linguagens Ln e Lm: ";
+        cin >> N;
+
+        GetLanguageFromState(N);
+
+        cout << "Ln: ";
+        ShowLanguage(Ln);
+        cout << "Lm: ";
+        ShowLanguage(Lm);
+
+        cout<< "Deseja verificar com outro valor N? [y/n] ";
+        cin >> input;
+        if(input != "y" && input != "yes" && input != "s" && input != "sim")
+            ans = !ans;
+        else{
+            Ln.clear();
+            Lm.clear();
+        }
+    }while(ans);
+    return 0;
+}
